94ff79d1b161043baadc3ccaf5d93eb03f3ad924
Cammille Carinan <cammille.carinan@xfel.eu>
2023-06-18 18:41:20 +0200

    FEAT: Display array cell

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,6 +3,7 @@ import "@glideapps/glide-data-grid/dist/index.css";
 import React, { useCallback } from "react";
 import { connect } from "react-redux";
 import { DataEditor, GridCellKind } from "@glideapps/glide-data-grid";
+import { useExtraCells } from "@glideapps/glide-data-grid-cells";
 
 import { imageBytesToURL } from "../utils/helpers";
 
@@ -34,13 +35,31 @@ const numberCell = (data, params = {}) => {
   };
 };
 
+const arrayCell = (data, params = {}) => {
+  return {
+    kind: GridCellKind.Custom,
+    allowOverlay: false,
+    copyData: "4",
+    data: {
+      kind: "sparkline-cell",
+      values: data,
+      // displayValues: data.map((x) => Math.round(x).toString()), // TODO: Round in server?
+      color: "#77c4c4",
+      yAxis: [Math.min(...data), Math.max(...data)],
+    },
+  };
+};
+
 const gridCellFactory = {
   image: imageCell,
   string: textCell,
   number: numberCell,
+  array: arrayCell,
 };
 
 const Table = ({ data, columns, schema }) => {
+  const cellProps = useExtraCells();
+
   const getContent = useCallback(([col, row]) => {
     const column = columns[col].id;
     const rowData = data[row];
@@ -53,6 +72,7 @@ const Table = ({ data, columns, schema }) => {
         columns={columns}
         getCellContent={getContent}
         rows={data.length}
+        {...cellProps}
       />
       <div id="portal"></div>
     </div>
@@ -62,7 +82,7 @@ const Table = ({ data, columns, schema }) => {
 const mapStateToProps = ({ table }) => {
   const data = table.data ? Object.values(table.data) : [];
 
-  // TODO: Get the column list from the user settings (reorded columns)
+  // TODO: Get the column list from the user settings (reordered columns)
   const columns = table.schema
     ? Object.keys(table.schema).map((id) => ({ id, title: id }))
     : [];
