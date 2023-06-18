2099f8eba40be704b05028b0776657141f7d37d0
Cammille Carinan <cammille.carinan@xfel.eu>
2023-06-18 18:33:50 +0200

    FEAT: Add array data to table

--- src/cammille_api/main.py
+++ src/cammille_api/main.py
@@ -1,17 +1,21 @@
+from pathlib import Path
+
 import pandas as pd
 from fastapi import Depends, FastAPI
 from sqlalchemy import Connection, Select
 
 from .const import FILL_VALUE, Type
-from .db import get_column_datum, get_column_names, get_conn, get_selection
-from .utils import map_dtype, convert
+from .db import (
+    get_column_datum, get_column_names, get_conn, get_damnit_path,
+    get_selection)
+from .utils import convert, get_run_data, map_dtype
 
 app = FastAPI()
 
 
-def column_schema(
+def get_column_schema(
     column_names: list = Depends(get_column_names),
-    column_datum = Depends(get_column_datum),
+    column_data = Depends(get_column_datum),
     conn: Connection = Depends(get_conn)) -> dict: 
     """
     Returns a dictionary of the column names of the specified table and their data type.
@@ -26,17 +30,34 @@ def column_schema(
     - A dictionary of the column names of the specified table and their data type.
     """
     def get_dtype(column, default='string'):
-        series =  pd.read_sql(column_datum(column), conn)[column]
+        series =  pd.read_sql(column_data(column), conn)[column]
         return (map_dtype(type(series[0]), default).value
                 if not series.empty else default)
     
-    return {column: get_dtype(column) for column in column_names}
+    schema = {column: get_dtype(column) for column in column_names}
+    # return schema
+
+    # REMOVEME: Get array from hardcoded column names
+    arrays = ['hrixs_spectrum']
+    return {**schema, **{col: Type.ARRAY for col in arrays}}
+
+
+def get_extracted_path(
+        proposal_number: str,
+        damnit_path: str = Depends(get_damnit_path),
+        ) -> str:
+    """Returns a function that generates the extracted data path"""
+    def inner(run_number):
+        return str(Path(damnit_path) / "extracted_data"
+                   / f"p{proposal_number}_r{run_number}.h5")
+    return inner
 
 
 @app.router.get("/db")
 def index(
     selection: Select = Depends(get_selection),
-    schema: dict = Depends(column_schema),
+    schema: dict = Depends(get_column_schema),
+    extracted_path = Depends(get_extracted_path),
     conn: Connection = Depends(get_conn),
 ) -> dict:
     """
@@ -44,6 +65,7 @@ def index(
 
     Parameters:
     - `selection`: A SQLAlchemy select statement with optional filters and pagination.
+    - `schema`: A dictionary of the column names of the specified table and their data type
     - `conn`: A SQLAlchemy connection instance for the specified engine.
 
     Returns:
@@ -52,16 +74,17 @@ def index(
     # Read the selected data from the database into a pandas DataFrame
     df = pd.read_sql(selection, conn)
 
-    # Attempt to convert any columns with dtype 'object' according to schema
+    # Convert columns according to some rules
     for k, v in df.dtypes.to_dict().items():
         dtype = Type(schema[k])
+        # Attempt to convert any columns with dtype 'object' according to schema
         if v == "object":
-            try:
-                df[k] = df[k].apply(lambda x: convert(x, dtype))
-            except Exception as e:
-                # If an error occurs, drop the column from the DataFrame
-                print(e)
-                df.drop(columns=k, inplace=True)
+            df[k] = df[k].apply(lambda x: convert(x, dtype))
+        # Fetch the extracted array from the run file
+        elif dtype == Type.ARRAY:
+            df[k] = df.apply(lambda row:
+                             get_run_data(extracted_path(row.runnr), k).tolist(),
+                             axis=1)
 
     # Fill any NaN values in the DataFrame with the string 'None'
     df.fillna(FILL_VALUE, inplace=True)
@@ -71,5 +94,5 @@ def index(
 
 
 @app.router.get("/db/schema")
-def db_schema(schema: dict = Depends(column_schema)) -> dict:
+def db_schema(schema: dict = Depends(get_column_schema)) -> dict:
     return schema
