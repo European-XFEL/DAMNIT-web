bfeda73d4d68508d81efe3895fa62c5af4fbe97d
Cammille Carinan <cammille.carinan@xfel.eu>
2023-06-19 22:49:51 +0200

    FEAT: Select table row

--- src/reducers/table.js
+++ src/reducers/table.js
@@ -1,8 +1,19 @@
 import { tableConstants } from "../constants";
 
-export default function table(state = {}, action) {
+const initialState = {
+  data: null,
+  schema: null,
+  selection: null,
+};
+
+export default function table(state = initialState, action) {
   switch (action.type) {
     case tableConstants.RECEIVE_TABLE:
+      return {
+        ...state,
+        ...action.payload,
+      };
+    case tableConstants.SELECT_ROW:
       return {
         ...state,
         ...action.payload,
