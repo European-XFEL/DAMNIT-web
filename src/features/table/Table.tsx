50eb8b9f07195a6206dcfa5cb52ba0633b6ef760
Cammille Carinan <cammille.carinan@xfel.eu>
2023-06-27 16:25:23 +0200

    FEAT: Initial cosmetic changes

--- src/features/table/Table.tsx
+++ src/features/table/Table.tsx
@@ -5,66 +5,18 @@ import { connect } from "react-redux";
 import {
   CompactSelection,
   DataEditor,
-  GridCellKind,
   GridSelection,
 } from "@glideapps/glide-data-grid";
 import { useExtraCells } from "@glideapps/glide-data-grid-cells";
 
+import { gridCellFactory } from "./cells";
 import ContextMenu from "./ContextMenu";
 
 import { selectRun } from "./tableSlice";
 import { addPlot } from "../plots";
 
-import { DTYPES, EMPTY_VALUE, RUN_NUMBER } from "../../common/constants";
-import { arrayEqual, imageBytesToURL, isEmpty } from "../../utils/helpers";
-
-const imageCell = (data, params = {}) => {
-  return {
-    kind: GridCellKind.Image,
-    allowOverlay: true,
-    data: [imageBytesToURL(data)],
-  };
-};
-
-const textCell = (data, params = {}) => {
-  return {
-    kind: GridCellKind.Text,
-    allowOverlay: false,
-    displayData: data !== EMPTY_VALUE ? data : "",
-    data,
-  };
-};
-
-const numberCell = (data, params = {}) => {
-  return {
-    kind: GridCellKind.Number,
-    allowOverlay: false,
-    displayData: data !== EMPTY_VALUE ? String(data) : "",
-    data,
-  };
-};
-
-const arrayCell = (data, params = {}) => {
-  return {
-    kind: GridCellKind.Custom,
-    allowOverlay: false,
-    copyData: "4",
-    data: {
-      kind: "sparkline-cell",
-      values: data,
-      // displayValues: TODO: Round in server?
-      color: "#77c4c4",
-      yAxis: [Math.min(...data), Math.max(...data)],
-    },
-  };
-};
-
-const gridCellFactory = {
-  image: imageCell,
-  string: textCell,
-  number: numberCell,
-  array: arrayCell,
-};
+import { DTYPES, VARIABLES } from "../../common/constants";
+import { arrayEqual, isEmpty } from "../../utils/helpers";
 
 const Table = (props) => {
   // Initialization: Use custom cells
@@ -91,7 +43,7 @@ const Table = (props) => {
     // Inform that a row has been (de)selected
     const row = rows.last();
     props.dispatch(
-      selectRun(isEmpty(row) ? null : props.data[row][RUN_NUMBER])
+      selectRun(isEmpty(row) ? null : props.data[row][VARIABLES.run_number])
     );
 
     // Clear range stack if cells from the other column are currently selected
@@ -119,6 +71,12 @@ const Table = (props) => {
   const [contextMenu, setContextMenu] = useState();
   const handleCellContextMenu = ([col, row], event) => {
     event.preventDefault();
+
+    // Ignore no selection for the meantime
+    if (!gridSelection.current) {
+      return;
+    }
+
     const cells = [
       gridSelection.current.cell,
       ...gridSelection.current.rangeStack.map((range) => [range.x, range.y]),
@@ -159,7 +117,9 @@ const Table = (props) => {
       props.dispatch(
         addPlot({
           variables: [props.columns[cell[0]].id],
-          runs: rows.map((row) => props.data[row][RUN_NUMBER]).toSorted(),
+          runs: rows
+            .map((row) => props.data[row][VARIABLES.run_number])
+            .toSorted(),
         })
       );
     } else {
@@ -167,7 +127,9 @@ const Table = (props) => {
       props.dispatch(
         addPlot({
           variables: [props.columns[col].id],
-          runs: props.data.map((rowData) => rowData[RUN_NUMBER]).toSorted(),
+          runs: props.data
+            .map((rowData) => rowData[VARIABLES.run_number])
+            .toSorted(),
         })
       );
     }
@@ -180,12 +142,24 @@ const Table = (props) => {
     },
   ];
 
+  // Format columns
+  const formattedColumns = props.columns.map((column) => ({
+    ...column,
+    ...(props.schema[column.id].dtype === DTYPES.number && {
+      width: 100,
+      themeOverride: {
+        fontFamily: "monospace",
+        headerFontStyle: "",
+      },
+    }),
+  }));
+
   return (
     <div>
       {!props.columns.length ? null : (
         <>
           <DataEditor
-            columns={props.columns}
+            columns={formattedColumns}
             getCellContent={getContent}
             rows={props.data.length}
             rowSelect="single"
@@ -195,6 +169,7 @@ const Table = (props) => {
             rangeSelect="multi-cell"
             onCellContextMenu={handleCellContextMenu}
             onHeaderContextMenu={handleHeaderContextMenu}
+            freezeColumns={1}
             {...cellProps}
           />
           {contextMenu && (
@@ -215,7 +190,9 @@ const mapStateToProps = ({ table }) => {
   const data = table.data ? Object.values(table.data) : [];
 
   // TODO: Get the column list from the user settings (reordered columns)
-  const columns = Object.keys(table.schema).map((id) => ({ id, title: id }));
+  const columns = Object.keys(table.schema)
+    .filter((id) => id !== VARIABLES.proposal)
+    .map((id) => ({ id, title: id }));
 
   return {
     data,
