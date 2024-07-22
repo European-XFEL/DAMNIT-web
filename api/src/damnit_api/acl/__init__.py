5a2df81a3268b6e15c55121e4c690cd2e777ecc7
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2024-07-22 10:35:32 +0200

    feat(api/acl): add models mapping nfs acls to python

---
+++ api/src/damnit_api/acl/__init__.py
@@ -0,0 +1,6 @@
+"""Module mapping GPFS NFSv4 ACLs to python.
+
+This module provides some models which map a few concepts from the GPFS NFSv4 ACLs to
+python objects, for the purpose of checking a users permissions based on a GPFS
+directory or file.
+"""
