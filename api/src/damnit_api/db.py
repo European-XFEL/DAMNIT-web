766512f62b3584d41b35e7486dcbeb62112779db
Cammille Carinan <cammille.carinan@xfel.eu>
2024-07-24 11:40:28 +0200

    refactor(rest): remove `db` Rest API implementation (#9)
    * refactor(rest): remove Rest API implementation for the meantime
    
    * refactor(rest): remove frontend test setup

--- api/src/damnit_api/db.py
+++ api/src/damnit_api/db.py
@@ -4,16 +4,11 @@ from typing import AsyncIterator
 from pathlib import Path
 from typing import Union
 
-from fastapi import Depends
 from sqlalchemy import (
-    Engine,
     MetaData,
-    Select,
     Table,
-    create_engine,
     desc,
     func,
-    inspect,
     select,
 )
 from sqlalchemy.ext.asyncio import (
@@ -140,7 +135,7 @@ def get_extracted_data(proposal, run, variable):
 
 
 # -----------------------------------------------------------------------------
-# Synchronous
+# Etc.
 
 
 def get_damnit_path(proposal_number: str = DEFAULT_PROPOSAL) -> str:
@@ -149,70 +144,3 @@ def get_damnit_path(proposal_number: str = DEFAULT_PROPOSAL) -> str:
     if not path:
         raise RuntimeError(f"Proposal '{proposal_number}' is not found.")
     return str(Path(path) / DAMNIT_PATH)
-
-
-def get_engine(
-    damnit_path: str = Depends(get_damnit_path),
-) -> Engine:
-    """
-    Returns a SQLAlchemy engine instance for the specified database file.
-    """
-    db_path = str(Path(damnit_path) / 'runs.sqlite')
-    engine = create_engine(f"sqlite:///{db_path}")
-    return engine
-
-
-def get_conn(engine: Engine = Depends(get_engine)):
-    """
-    Returns a SQLAlchemy connection instance for the specified engine.
-    """
-    with engine.connect() as conn:
-        yield conn
-
-
-def get_table(
-    engine: Engine = Depends(get_engine),
-    table_name: str = "runs"
-) -> Table:
-    """Returns a SQLAlchemy table for the specified table name and engine."""
-    return Table(table_name, MetaData(), autoload_with=engine)
-
-
-def get_base_selection(table: Table = Depends(get_table)) -> Select:
-    """
-    Returns a base SQLAlchemy select statement for the specified table name and engine.
-    """
-    return select(table)
-
-
-def get_selection(
-    selection: Select = Depends(get_base_selection),
-    run_number: int = None,
-    page_size: int = 100,
-    offset: int = 0,
-):
-    """
-    Returns a SQLAlchemy select statement with optional filters and pagination.
-    """
-    if run_number:
-        selection = selection.filter_by(runnr=run_number)
-
-    return selection.offset(offset).limit(page_size)
-
-
-def get_column_names(
-    engine: Engine = Depends(get_engine),
-    table_name: str = "runs"
-):
-    """Returns a list of the column names for the specified table name and engine"""
-    return [column['name'] for column in inspect(engine).get_columns(table_name)]
-
-
-def get_column_datum(table: Table = Depends(get_table)):
-    """Returns a function which generates a SQLAlchemy select statement
-       that fetches one non-null value from the specified column"""
-    def inner(column_name):
-        column = table.c[column_name]
-        return select(column).where(column.is_not(None)).limit(1)
-
-    return inner
--- api/src/damnit_api/main.py
+++ api/src/damnit_api/main.py
@@ -1,18 +1,10 @@
 from contextlib import asynccontextmanager
-from starlette.middleware.sessions import SessionMiddleware
-from pathlib import Path
 
-import pandas as pd
 from fastapi import (
-    APIRouter, Depends, FastAPI, HTTPException, Request, status)
+    Depends, FastAPI, HTTPException, Request, status)
 from fastapi.responses import JSONResponse, RedirectResponse
-from sqlalchemy import Connection, Select
+from starlette.middleware.sessions import SessionMiddleware
 
-from .const import DEFAULT_PROPOSAL, FILL_VALUE, Type
-from .db import (
-    get_column_datum, get_column_names, get_conn, get_damnit_path,
-    get_selection)
-from .utils import convert, get_run_data, map_dtype
 from .graphql import add_graphql_router
 from . import auth
 from .settings import settings
@@ -27,7 +19,6 @@ KNOWN_PATHS = ['/graphql']
 async def lifespan(app: FastAPI):
     auth.configure()
     add_graphql_router(app, dependencies=[Depends(auth.check_auth)])
-    app.router.include_router(db_router)
     app.router.include_router(auth.router)
     yield
 
@@ -44,106 +35,6 @@ async def http_exception_handler(request: Request, exc: HTTPException):
         status_code=exc.status_code,
         content={"detail": exc.detail},
     )
-
-
-def get_column_schema(
-    column_names: list = Depends(get_column_names),
-    column_data = Depends(get_column_datum),
-    conn: Connection = Depends(get_conn)) -> dict:
-    """
-    Returns a dictionary of the column names of the specified table and their data type.
-
-    Parameters:
-    - `column_names`: A list of the column names for the specified table name and engine.
-    - `column_datum`: A function that generates a SQLAlchemy select statement
-       that fetches one non-null value from the specified column
-    - `conn`: A SQLAlchemy connection instance for the specified engine.
-
-    Returns:
-    - A dictionary of the column names of the specified table and their data type.
-    """
-    def get_dtype(column, default='string'):
-        series =  pd.read_sql(column_data(column), conn)[column]
-        return (map_dtype(type(series[0]), default).value
-                if not series.empty else default)
-
-    schema = {column: {'id': column, 'dtype': get_dtype(column)}
-              for column in column_names}
-
-    # REMOVEME: Get dtype from hardcoded column names
-    known_dtypes = {
-        'start_time': Type.TIMESTAMP,
-        'added_at': Type.TIMESTAMP,
-        'hrixs_spectrum': Type.ARRAY,
-    }
-    for name, dtype in known_dtypes.items():
-        schema[name]['dtype'] = dtype
-
-    return schema
-
-
-def get_extracted_path(
-        proposal_number: str = DEFAULT_PROPOSAL,
-        damnit_path: str = Depends(get_damnit_path),
-        ) -> str:
-    """Returns a function that generates the extracted data path"""
-    def inner(run_number):
-        return str(Path(damnit_path) / "extracted_data"
-                   / f"p{proposal_number}_r{run_number}.h5")
-    return inner
-
-
-db_router = APIRouter(
-    prefix="/db", dependencies=[Depends(auth.check_auth)]
-)
-
-
-@db_router.get("/db")
-def index(
-    selection: Select = Depends(get_selection),
-    schema: dict = Depends(get_column_schema),
-    extracted_path = Depends(get_extracted_path),
-    conn: Connection = Depends(get_conn),
-) -> dict:
-    """
-    Returns a list of dictionaries representing the rows in the specified database table.
-
-    Parameters:
-    - `selection`: A SQLAlchemy select statement with optional filters and pagination.
-    - `schema`: A dictionary of the column names of the specified table and their data type
-    - `conn`: A SQLAlchemy connection instance for the specified engine.
-
-    Returns:
-    - A list of dictionaries representing the rows in the specified database table.
-    """
-    # Read the selected data from the database into a pandas DataFrame
-    df = pd.read_sql(selection, conn)
-
-    # Convert columns according to some rules
-    for k, v in df.dtypes.to_dict().items():
-        dtype = Type(schema[k]['dtype'])
-        # Attempt to convert any columns with dtype 'object' according to schema
-        if v == "object":
-            df[k] = df[k].apply(lambda x: convert(x, dtype))
-        # Fetch the extracted array from the run file
-        elif dtype == Type.ARRAY:
-            df[k] = df.apply(lambda row:
-                             get_run_data(extracted_path(row.runnr), k).tolist(),
-                             axis=1)
-        elif dtype == Type.TIMESTAMP:
-            # Convert from UNIX to EPOCH timestamp
-            df[k] = df[k].apply(lambda t: t * 1000)
-
-    # Fill any NaN values in the DataFrame with the string 'None'
-    df.fillna(FILL_VALUE, inplace=True)
-
-    # Convert the DataFrame to a dictionary with run number as key
-    return df.set_index("runnr", drop=False).to_dict(orient="index")
-
-
-@db_router.get("/db/schema")
-def db_schema(schema: dict = Depends(get_column_schema)) -> dict:
-    return schema
 
 
 app.add_middleware(
