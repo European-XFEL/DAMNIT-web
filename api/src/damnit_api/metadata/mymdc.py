ae22b19994ecb94ae95947e74ba4326506ecccc7
Cammille Carinan <cammille.carinan@xfel.eu>
2025-02-05 13:22:03 +0100

    feat(api/mymdc): fetch MyMDC queries asynchronously

--- api/src/damnit_api/metadata/mymdc.py
+++ api/src/damnit_api/metadata/mymdc.py
@@ -1,56 +1,72 @@
-import requests
-from dotenv import dotenv_values
+import aiohttp
 
 from .. import get_logger
 from ..settings import settings
 
 logger = get_logger()
 
-ENV_VARS = dotenv_values()
-
 
 class MyMDC:
-    HEADERS = {
-        "accept": "application/json; version=1",
-        "Content-Type": "application/json",
-    }
-
-    token: str
+    HEADERS = (
+        ("accept", "application/json; version=1"),
+        ("Content-Type", "application/json"),
+    )
 
     def __init__(self):
-        logger.info("Initializing MyMDC client")
-        self.fetch_token()
+        self.session = None
+        self.token = None
 
-    def fetch_token(self):
+    async def __aenter__(self):
+        timeout = aiohttp.ClientTimeout(total=10)  # seconds
+        self.session = aiohttp.ClientSession(timeout=timeout)
+
+        await self.fetch_token()
+        return self
+
+    async def __aexit__(self, exc_type, exc_value, traceback):
+        await self.session.close()
+
+    async def fetch_token(self):
+        """
+        Fetch the OAuth token asynchronously.
+        """
         url = f"{settings.mymdc.base_url}/api/../oauth/token"
-        logger.debug("Fetching MyMDC token", url=url)
+        logger.debug("Fetching MyMDC token", extra={"url": url})
         body = {
             "grant_type": "client_credentials",
             "client_id": settings.mymdc.client_id,
             "client_secret": settings.mymdc.client_secret.get_secret_value(),
         }
 
-        response = requests.post(url, headers=self.HEADERS, json=body)
-        response.raise_for_status()
+        async with self.session.post(
+            url, headers=dict(self.HEADERS), json=body
+        ) as response:
+            response.raise_for_status()
+            data = await response.json()
 
-        data = response.json()
+            self.token = data["access_token"]
+            return self.token
 
-        self.token = data["access_token"]
-        return self.token
+    async def fetch_proposal_info(self, proposal_num: str):
+        """
+        Fetch proposal info given a proposal number.
+        """
+        url = (
+            f"{settings.mymdc.base_url}/api/proposals/by_number/{proposal_num}"
+        )
+        async with self.session.get(url, headers=self.headers) as response:
+            response.raise_for_status()
+            return await response.json()
 
-    def fetch_proposal_info(self, proposal_num):
-        url = f"{settings.mymdc.base_url}/api/proposals/by_number/{proposal_num}"
-        response = requests.get(url, headers=self.headers)
-        response.raise_for_status()
-
-        return response.json()
-
-    def fetch_user(self, user_id):
+    async def fetch_user(self, user_id: str):
+        """
+        Fetch user info given a user ID.
+        """
         url = f"{settings.mymdc.base_url}/api/users/{user_id}"
-        response = requests.get(url, headers=self.headers)
-        response.raise_for_status()
-        return response.json()
+        async with self.session.get(url, headers=self.headers) as response:
+            response.raise_for_status()
+            return await response.json()
 
     @property
     def headers(self):
-        return {**self.HEADERS, "Authorization": f"Bearer {self.token}"}
+        return {**dict(self.HEADERS), "Authorization": f"Bearer {self.token}"}
