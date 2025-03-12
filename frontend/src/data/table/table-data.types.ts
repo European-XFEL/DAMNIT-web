c2cbc0c3fcaebe7b4c3e267ec4c66fad8748dcbd
Cammille Carinan <cammille.carinan@xfel.eu>
2025-03-12 14:35:28 +0100

    refactor(frontend): convert `data` to TypeScript

---
+++ frontend/src/data/table/table-data.types.ts
@@ -0,0 +1,28 @@
+import { VariableDataItem, VariableMetadataItem } from "../../types"
+
+export type TableData = {
+  [run: string]: { [variable: string]: VariableDataItem }
+}
+
+export type TableMetadata = {
+  variables: Record<string, VariableMetadataItem>
+  runs: string[]
+  timestamp: number
+}
+
+export type TableInfo = {
+  data: TableData
+  metadata: TableMetadata
+}
+
+export type TableDataOptions = {
+  proposal: string
+  page?: number
+  pageSize?: number
+  lightweight?: boolean
+  deferred?: boolean
+}
+
+export type TableMetadataOptions = {
+  proposal: string
+}
