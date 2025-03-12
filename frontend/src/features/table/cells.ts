a993c9f5703dae6c13f716a61e32ef01e7d23eea
Cammille Carinan <cammille.carinan@xfel.eu>
2025-03-12 15:29:51 +0100

    refactor(frontend): convert `features/table` to TypeScript

--- frontend/src/features/table/cells.ts
+++ frontend/src/features/table/cells.ts
@@ -1,40 +1,65 @@
-import { GridCellKind } from "@glideapps/glide-data-grid"
+import {
+  BaseGridCell,
+  GridCell,
+  GridCellKind,
+  ImageCell,
+  Item,
+  LoadingCell,
+  NumberCell,
+  TextCell,
+} from "@glideapps/glide-data-grid"
+import { SparklineCellType } from "@glideapps/glide-data-grid-cells"
+
 import { DTYPES } from "../../constants"
-import { formatDate, formatFloat } from "../../utils/helpers"
+import { VariableValue } from "../../types"
+import { formatDate, formatNumber } from "../../utils/helpers"
 
 // TODO: Handle nonconforming data type
 
-export const imageCell = (data, params = {}) => {
-  const image = data ? [data] : []
+export const imageCell = (
+  value: VariableValue,
+  params: Partial<BaseGridCell> = {},
+): ImageCell => {
+  const data = typeof value === "string" ? [value] : []
+
   return {
     kind: GridCellKind.Image,
-    data: image,
+    data,
     allowOverlay: false,
-    allowAdd: false,
+    // allowAdd: false,
     readonly: true,
     ...params,
   }
 }
 
-export const textCell = (data, params = {}) => {
+export const textCell = (
+  value: VariableValue,
+  params: Partial<BaseGridCell> = {},
+): TextCell => {
+  const data = value ? String(value) : ""
   return {
     kind: GridCellKind.Text,
-    displayData: data ? String(data) : "",
+    displayData: data,
     data,
     allowOverlay: false,
     ...params,
   }
 }
 
-export const numberCell = (data, params = {}) => {
+export const numberCell = (
+  value: VariableValue,
+  params: Partial<BaseGridCell> = {},
+): NumberCell => {
+  const data =
+    typeof value === "number" && Number.isFinite(value)
+      ? formatNumber(value)
+      : undefined
+
   return {
     kind: GridCellKind.Number,
-    displayData: Number.isFinite(data)
-      ? String(Number.isInteger(data) ? data : formatFloat(data))
-      : data !== null
-      ? String(data)
-      : "",
-    data,
+    displayData:
+      data != null ? String(data) : value != null ? String(value) : "",
+    data: data,
     allowOverlay: false,
     contentAlign: "right",
     themeOverride: {
@@ -45,27 +70,42 @@ export const numberCell = (data, params = {}) => {
   }
 }
 
-export const arrayCell = (data, params = {}) => {
+export const arrayCell = (
+  value: VariableValue,
+  params: Partial<BaseGridCell> = {},
+): SparklineCellType => {
+  const values = Array.isArray(value) ? (value as number[]) : []
+
+  const data = value
+    ? {
+        values: values,
+        yAxis: [Math.min(...values), Math.max(...values)] as Item,
+      }
+    : { values: [], yAxis: [0, 0] as Item }
+
   return {
     kind: GridCellKind.Custom,
     allowOverlay: false,
-    copyData: "4",
+    copyData: "",
     data: {
       kind: "sparkline-cell",
-      values: data,
-      // displayValues: TODO: Round in server?
       color: "#77c4c4",
-      yAxis: [Math.min(...data), Math.max(...data)],
+      ...data,
     },
     ...params,
   }
 }
 
-export const dateCell = (data, params = {}) => {
+export const dateCell = (
+  value: VariableValue,
+  params: Partial<BaseGridCell> = {},
+): TextCell => {
+  const data = value && typeof value === "number" ? formatDate(value) : ""
+
   return {
     kind: GridCellKind.Text,
     allowOverlay: false,
-    displayData: data ? formatDate(data) : "",
+    displayData: data,
     data,
     themeOverride: {
       fontFamily: "monospace",
@@ -74,7 +114,10 @@ export const dateCell = (data, params = {}) => {
   }
 }
 
-export const loadingCell = (data, params = {}) => {
+export const loadingCell = (
+  _: VariableValue,
+  params: Partial<BaseGridCell> = {},
+): LoadingCell => {
   return {
     kind: GridCellKind.Loading,
     allowOverlay: false,
@@ -92,6 +135,18 @@ const gridCellFactory = {
   [DTYPES.timestamp]: dateCell,
 }
 
-export const getCell = (value, dtype) => {
-  return value !== null ? gridCellFactory[dtype] ?? textCell("") : loadingCell
+type GetCellOptions = {
+  value: VariableValue
+  dtype: keyof typeof gridCellFactory
+  options: Partial<BaseGridCell>
+}
+
+export const getCell = ({
+  value,
+  dtype,
+  options,
+}: GetCellOptions): GridCell => {
+  // If the value is null or undefined, use the loading cell factory.
+  const factory = value == null ? loadingCell : gridCellFactory[dtype]
+  return factory(value, options)
 }
