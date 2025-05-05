543e2faf1897ae1da5b67114dc78852c30734faa
Matheus Teodoro <matheus.teodoro@xfel.eu>
2025-05-05 09:32:15 +0200

    first-commit

--- frontend/src/redux/listeners.ts
+++ frontend/src/redux/listeners.ts
@@ -2,6 +2,8 @@ import { createListenerMiddleware } from "@reduxjs/toolkit"
 
 import { addTab, removeTab, openAside, closeAside } from "../features/dashboard"
 import { addPlot, removePlot, resetPlots } from "../features/plots/"
+import { openEditor, resetEditor, clearUnseenChanges } from "../features/editor"
+import { setCurrentTab } from "../features/dashboard/dashboard.slice"
 import { selectRun } from "../features/table"
 import { RootState } from "../redux/reducer"
 import { isEmpty } from "../utils/helpers"
@@ -22,6 +24,35 @@ listenerMiddleware.startListening({
   actionCreator: addPlot,
   effect: (action, { dispatch }) => {
     dispatch(addTab({ id: "plots", title: "Plots", isClosable: true }))
+  },
+})
+
+listenerMiddleware.startListening({
+  actionCreator: openEditor,
+  effect: (action, { dispatch }) => {
+    dispatch(addTab({ id: "editor", title: "Context File", isClosable: true }))
+  },
+})
+
+listenerMiddleware.startListening({
+  actionCreator: removeTab,
+  effect: (action, { dispatch }) => {
+    const id = action.payload
+    if (id === "plots") {
+      dispatch(resetPlots())
+    } else if (id === "editor") {
+      dispatch(resetEditor())
+    }
+  },
+})
+
+listenerMiddleware.startListening({
+  actionCreator: setCurrentTab,
+  effect: (action, { dispatch, getState }) => {
+    const tabId = action.payload
+    if (tabId === 'editor') {
+      dispatch(clearUnseenChanges())
+    }
   },
 })
 
