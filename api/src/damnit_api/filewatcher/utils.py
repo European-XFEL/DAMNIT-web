19e9d36439e31b7769cac273717276d8619a47b4
Matheus Teodoro <matheus.teodoro@xfel.eu>
2025-05-12 16:54:00 +0200

    Adds aiofiles to fetch_file_data

--- api/src/damnit_api/filewatcher/utils.py
+++ api/src/damnit_api/filewatcher/utils.py
@@ -1,19 +1,15 @@
-import hashlib
-import os
+import aiofiles
 import logging
 import asyncio
 from pathlib import Path
 
 async def fetch_file_data(file_path: Path, with_content=False):
     attempts = 3
-    data = {"checksum": None, "fileContent": None, "lastModified": None}
     for _ in range(attempts):
         try:
-            with open(file_path, 'r', encoding='utf-8') as file:
-                content = file.read()
-            return content
-
+            async with aiofiles.open(file_path, 'r', encoding='utf-8') as f:
+                return await f.read()
         except (OSError, IOError) as e:
-            logging.warning(f"Error reading file {file_path}, retrying... {e}")
+            logging.warning(f"Error reading {file_path!r}, retrying… {e}")
             await asyncio.sleep(0.1)
     return ""
