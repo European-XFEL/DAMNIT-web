b28937c88b921bcc38b862f4352ade82473fbe92
Matheus Teodoro <matheus.teodoro@xfel.eu>
2025-09-26 11:59:40 +0200

    refactor: use renderer map for view selection

--- frontend/src/features/user-settings/user-settings.tsx
+++ frontend/src/features/user-settings/user-settings.tsx
@@ -22,98 +22,64 @@ export type SettingsView =
   | 'visibility-all'
   | 'visibility-tags'
 
+const viewRenderers: Record<SettingsView, (ctx: {
+  setCurrentView: (v: SettingsView) => void
+  hasTags: boolean
+}) => React.ReactNode> = {
+  'visibility-all': () => <VisibilitySettings variant="all-variables" />,
+  'visibility-tags': () => (
+    <>
+      <Flex align="center" justify="between" gap="md" flex="column" />
+      <VisibilitySettings variant="tag-variables" />
+    </>
+  ),
+  visibility: ({ setCurrentView, hasTags }) => (
+    <>
+      <Divider label=" Column Visibility" labelPosition="center" size="lg" />
+      <UnstyledButton onClick={() => setCurrentView('visibility-all')}>
+        <Group justify="space-between">
+          <Text>By Variables</Text>
+          <IconChevronRight style={{ width: rem(20), height: rem(20) }} stroke={1.5} />
+        </Group>
+      </UnstyledButton>
+      <UnstyledButton
+        disabled={!hasTags}
+        onClick={() => setCurrentView('visibility-tags')}
+      >
+        <Group justify="space-between">
+          <Group>
+            <Text size="md" c={!hasTags ? 'dimmed' : undefined}>By Tags</Text>
+          </Group>
+          <IconChevronRight style={{ width: rem(20), height: rem(20) }} stroke={1.5} />
+        </Group>
+      </UnstyledButton>
+      <Divider />
+    </>
+  ),
+  main: ({ setCurrentView }) => (
+    <>
+      <Divider label="" labelPosition="center" size="lg" />
+      <UnstyledButton onClick={() => setCurrentView('visibility')}>
+        <Group justify="space-between">
+          <Text>Visibility</Text>
+          <IconChevronRight style={{ width: rem(20), height: rem(20) }} stroke={1.5} />
+        </Group>
+      </UnstyledButton>
+      <Divider />
+    </>
+  ),
+}
+
 function UserSettings() {
   const [currentView, setCurrentView] = useState<SettingsView>('main')
-
   const hasTags = useAppSelector(
     (state) => !!Object.keys(state.tableData.metadata.tags).length
   )
 
-  if (currentView === 'visibility-all') {
-    return (
-      <Stack h="100%">
-        <BreadcrumbsBar
-          currentView={currentView}
-          onNavigate={setCurrentView}
-        />
-        <VisibilitySettings variant="all-variables" />
-      </Stack>
-    )
-  }
-
-  if (currentView === 'visibility-tags') {
-    return (
-      <Stack h="100%">
-        <BreadcrumbsBar
-          currentView={currentView}
-          onNavigate={setCurrentView}
-        />
-        <Flex align="center" justify="between" gap="md" flex="column"></Flex>
-        <VisibilitySettings variant="tag-variables" />
-      </Stack>
-    )
-  }
-
-  if (currentView === 'visibility') {
-    return (
-      <Stack h="100%">
-        <BreadcrumbsBar
-          currentView={currentView}
-          onNavigate={setCurrentView}
-        />
-        <Divider label=" Column Visibility" labelPosition="center" size="lg" />
-
-        <UnstyledButton onClick={() => setCurrentView('visibility-all')}>
-          <Group justify="space-between">
-            <Text>By Variables</Text>
-            <IconChevronRight
-              style={{ width: rem(20), height: rem(20) }}
-              stroke={1.5}
-            />
-          </Group>
-        </UnstyledButton>
-
-        <UnstyledButton
-          disabled={!hasTags}
-          onClick={() => setCurrentView('visibility-tags')}
-        >
-          <Group justify="space-between">
-            <Group>
-              <Text size="md" c={!hasTags ? 'dimmed' : undefined}>
-                By Tags
-              </Text>
-            </Group>
-            <IconChevronRight
-              style={{ width: rem(20), height: rem(20) }}
-              stroke={1.5}
-            />
-          </Group>
-        </UnstyledButton>
-
-        <Divider />
-      </Stack>
-    )
-  }
-
   return (
-    <Stack>
-      <BreadcrumbsBar
-        currentView={currentView}
-        onNavigate={setCurrentView}
-      />
-      <Divider label="" labelPosition="center" size="lg" />
-
-      <UnstyledButton onClick={() => setCurrentView('visibility')}>
-        <Group justify="space-between">
-          <Text>Visibility</Text>
-          <IconChevronRight
-            style={{ width: rem(20), height: rem(20) }}
-            stroke={1.5}
-          />
-        </Group>
-      </UnstyledButton>
-
-      <Divider />
+    <Stack h="100%">
+      <BreadcrumbsBar currentView={currentView} onNavigate={setCurrentView} />
+      {viewRenderers[currentView]({ setCurrentView, hasTags })}
     </Stack>
   )
 }
