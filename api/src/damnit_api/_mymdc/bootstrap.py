d19d6c130717816f42d1719a74e0b21df361447c
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2025-11-13 08:38:59 +0100

    chore(api/_mymdc): minor changes

--- api/src/damnit_api/_mymdc/bootstrap.py
+++ api/src/damnit_api/_mymdc/bootstrap.py
@@ -20,25 +20,25 @@ async def bootstrap(settings: "Settings"):
     warning."""
     import damnit_api._mymdc
 
-    if damnit_api._mymdc.__CLIENT is None:
+    if damnit_api._mymdc.CLIENT is None:
         await logger.ainfo("Initialising MyMdC client")
-        damnit_api._mymdc.__CLIENT = await _init(settings)
+        damnit_api._mymdc.CLIENT = await _init(settings)
     else:
         await logger.awarning("MyMdC client already initialised")
 
 
 async def _init(
     settings: "Settings",
-) -> clients.MyMdCClient | clients.MockMyMdCClient:
+) -> clients.MyMdCClient | clients.MyMdCClientMock:
     """Create MyMdC client based on settings."""
     match settings.mymdc:
         case MyMdCCredentials():
             await logger.ainfo("Creating MyMdC client from credentials")
-            auth = clients.MyMdCAuth.model_validate(settings.mymdc)
+            auth = clients.MyMdCAuth.model_validate(settings.mymdc.model_dump())
             return clients.MyMdCClient(auth)
         case MockMyMdCData():
             await logger.ainfo("Creating Mock MyMdC client")
-            return clients.MockMyMdCClient.model_validate(settings.mymdc.model_dump())
+            return clients.MyMdCClientMock.model_validate(settings.mymdc.model_dump())
         case _:
             msg = "Invalid MyMdC configuration"
             raise ValueError(msg)
