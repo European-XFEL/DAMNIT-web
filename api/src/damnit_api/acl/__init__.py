568b2001ac3fab7e69127580c747c194c3835712
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2025-11-13 08:42:01 +0100

    chore(api): remove old code

--- api/src/damnit_api/acl/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Module mapping GPFS NFSv4 ACLs to python.
-
-This module provides some models which map a few concepts from the GPFS NFSv4
-ACLs to Python objects, for the purpose of checking a users permissions based
-on a GPFS directory or file.
-"""
