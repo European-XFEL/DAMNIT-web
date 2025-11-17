6b431c3b82ddf7e61bfbda2c5104d7766be257bc
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2025-11-17 23:32:38 +0100

    feat(api/_mymdc): use json for mock mymdc response data

--- api/src/damnit_api/_mymdc/settings.py
+++ api/src/damnit_api/_mymdc/settings.py
@@ -2,12 +2,8 @@
 
 from datetime import UTC, datetime
 
-from pydantic import HttpUrl, SecretStr
+from pydantic import FilePath, HttpUrl, SecretStr
 from pydantic_settings import BaseSettings
-
-from damnit_api._mymdc import models
-
-from .vendor.models import UsersProposal
 
 
 class MyMdCCredentials(BaseSettings):
@@ -27,45 +23,10 @@ class MyMdCCredentials(BaseSettings):
     _expires_at: datetime = datetime.fromisocalendar(1970, 1, 1).astimezone(UTC)
 
 
-# TODO: Allow loading mock data from files
 class MockMyMdCData(BaseSettings):
     """Mock MyMdC data for testing and local development."""
 
-    mock_users: dict[str, models.User] = {
-        "foo": models.User(email="foo@bar.com", first_name="Foo", last_name="Bar"),
-    }
-
-    mock_proposals: dict[int, models.Proposal] = {
-        1234: models.Proposal(
-            number=1234,
-            def_proposal_path="/gpfs/exfel/exp/SCS/202131/p900212/raw",
-            beamtime_start_at="2020-01-01T00:00:00+00:00",
-            beamtime_end_at="2020-01-01T00:00:00+00:00",
-            instrument_identifier="SCS",
-            title="Foo",
-            instrument_id=1234,
-            instrument_cycle_id=1234,
-            principal_investigator_id=1234,
-            main_proposer_id=1234,
-        )
-    }
-
-    mock_cycles: dict[int, models.InstrumentCycle] = {
-        1234: models.InstrumentCycle(
-            identifier="202020",
-            instrument_id=1234,
-            begin_at="2020-01-01T00:00:00+00:00",
-            end_at="2020-01-01T00:00:00+00:00",
-        )
-    }
-
-    mock_user_proposals: dict[str, models.UserProposals] = {
-        "roscar": models.UserProposals(
-            root=[
-                UsersProposal(proposal_id=1234, proposal_number=1234),
-            ],
-        )
-    }
+    mock_responses_file: FilePath
 
 
 type MyMdCConfig = MyMdCCredentials | MockMyMdCData
