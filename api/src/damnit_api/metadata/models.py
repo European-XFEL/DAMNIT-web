a442d620cc2869dd56de548fac4efeb67746bb17
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2025-11-18 10:41:23 +0100

    feat(api/metadata): split return types to show damnit path is definitely set

--- api/src/damnit_api/metadata/models.py
+++ api/src/damnit_api/metadata/models.py
@@ -3,12 +3,12 @@
 from datetime import datetime
 from pathlib import Path
 
-from pydantic import BaseModel
+from pydantic import BaseModel, DirectoryPath
 
 from ..shared.models import ProposalCycle, ProposalNo
 
 
-class ProposalMeta(BaseModel):
+class ProposalMetaBase(BaseModel):
     """Proposal Metadata."""
 
     no: ProposalNo
@@ -17,8 +17,18 @@ class ProposalMeta(BaseModel):
     path: Path
     title: str
 
+    start_date: datetime | None
+    end_date: datetime | None
+
+
+class ProposalMeta(ProposalMetaBase):
     damnit_path: Path | None = None
     damnit_paths_searched: list[Path]
 
-    start_date: datetime | None
-    end_date: datetime | None
+    def with_damnit_path(self) -> "ProposalMetaWithPath":
+        return ProposalMetaWithPath(**self.model_dump())
+
+
+class ProposalMetaWithPath(ProposalMetaBase):
+    damnit_path: DirectoryPath
+    damnit_paths_searched: list[Path]
