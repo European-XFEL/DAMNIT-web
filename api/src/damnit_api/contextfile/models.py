3897ea97ede1a1b9877975ef42b747484e274ee8
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2026-01-18 13:45:35 +0100

    revert(api/contextfile): respond with what the frontend expects
    done to avoid extra frontend chanegs

--- api/src/damnit_api/contextfile/models.py
+++ api/src/damnit_api/contextfile/models.py
@@ -1,4 +1,3 @@
-from datetime import UTC, datetime
 from typing import Self
 
 import async_lru
@@ -7,26 +6,50 @@ from pydantic import BaseModel
 
 
 class ModifiedTime(BaseModel):
-    unix_timestamp: float
-    time: datetime
+    lastModified: float  # noqa: N815
 
     @classmethod
     @async_lru.alru_cache(ttl=5)
     async def from_file(cls, path: APath) -> Self:
         stat = await path.stat()
-        return cls(
-            unix_timestamp=stat.st_mtime,
-            time=datetime.fromtimestamp(stat.st_mtime, tz=UTC),
-        )
+        return cls(lastModified=stat.st_mtime)
 
 
 class ContextFile(BaseModel):
-    modified_time: ModifiedTime
-    content: str
+    lastModified: float  # noqa: N815
+    fileContent: str  # noqa: N815
 
     @classmethod
     @async_lru.alru_cache(ttl=5)
     async def from_file(cls, path: APath) -> Self:
         content = await path.read_text()
-        modified_time = await ModifiedTime.from_file(path)
-        return cls(modified_time=modified_time, content=content)
+        modified_timestamp = await ModifiedTime.from_file(path)
+        return cls(lastModified=modified_timestamp.lastModified, fileContent=content)
+
+
+# TODO: update models to the following and update frontend API calls:
+#
+# class ModifiedTime(BaseModel):
+#     unix_timestamp: float
+#     time: datetime
+
+#     @classmethod
+#     @async_lru.alru_cache(ttl=5)
+#     async def from_file(cls, path: APath) -> Self:
+#         stat = await path.stat()
+#         return cls(
+#             unix_timestamp=stat.st_mtime,
+#             time=datetime.fromtimestamp(stat.st_mtime, tz=UTC),
+#         )
+
+
+# class ContextFile(BaseModel):
+#     modified_time: ModifiedTime
+#     content: str
+
+#     @classmethod
+#     @async_lru.alru_cache(ttl=5)
+#     async def from_file(cls, path: APath) -> Self:
+#         content = await path.read_text()
+#         modified_time = await ModifiedTime.from_file(path)
+#         return cls(modified_time=modified_time, content=content)
