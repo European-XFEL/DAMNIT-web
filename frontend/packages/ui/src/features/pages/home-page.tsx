f59f257dca1e7f5b8e1fb8188246abda8a0624b3
Cammille Carinan <cammille.carinan@xfel.eu>
2026-01-21 17:41:06 +0100

    feat(frontend/home); iuse gradient background on footer

--- frontend/packages/ui/src/features/pages/home-page.tsx
+++ frontend/packages/ui/src/features/pages/home-page.tsx
@@ -1,5 +1,5 @@
 import { type ReactNode } from 'react'
-import { AppShell } from '@mantine/core'
+import { AppShell, Stack } from '@mantine/core'
 
 import { SiteFooter } from '../../components/footers'
 
@@ -10,15 +10,21 @@ type HomePageProps = {
 
 const HomePage = ({ main, header }: HomePageProps) => {
   return (
-    <AppShell header={{ height: 60 }} padding="md">
-      <AppShell.Header>{header}</AppShell.Header>
+    <AppShell padding="md">
+      <AppShell.Header h={60}>{header}</AppShell.Header>
       <AppShell.Main>{main}</AppShell.Main>
       <AppShell.Footer
+        h={100}
         withBorder={false}
         mr={32}
-        style={{ background: 'transparent' }}
+        style={{
+          background:
+            'linear-gradient(180deg, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 70%)',
+        }}
       >
-        <SiteFooter />
+        <Stack h="100%" justify="flex-end">
+          <SiteFooter />
+        </Stack>
       </AppShell.Footer>
     </AppShell>
   )
