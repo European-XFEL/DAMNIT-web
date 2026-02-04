81a24879442af641845e07c817608f9e0314cad0
Cammille Carinan <cammille.carinan@xfel.eu>
2026-02-04 16:59:00 +0100

    feat(frontend/routes): use RootRoute on site

--- frontend/apps/site/src/main.tsx
+++ frontend/apps/site/src/main.tsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router'
+import { RouterProvider } from 'react-router'
 import { MantineProvider } from '@mantine/core'
 
 import '@damnit-frontend/ui/styles'
-import { routes } from './routes'
-
-const router = createBrowserRouter(routes, {
-  basename: import.meta.env.BASE_URL,
-})
+import { router } from './router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
