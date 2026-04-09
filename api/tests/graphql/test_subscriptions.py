7fc5d9b9e34e18cb6b0aab1490aadd65d7d899ec
Cammille Carinan <cammille.carinan@xfel.eu>
2026-04-09 16:17:34 +0200

    test(api/graphql): update tests for flat variable model

--- api/tests/graphql/test_subscriptions.py
+++ api/tests/graphql/test_subscriptions.py
@@ -4,20 +4,22 @@ from unittest.mock import patch
 
 import pytest
 
-from damnit_api.graphql.models import DamnitRun, serialize
+from damnit_api.graphql.models import DamnitRun
 from damnit_api.graphql.subscriptions import POLLING_INTERVAL, filter_for_client
+from damnit_api.shared.const import DamnitType
 
 from .const import (
     EXAMPLE_VARIABLES,
-    KNOWN_DTYPES,
-    KNOWN_VALUES,
-    NEW_DTYPES,
-    NEW_VALUES,
+    KNOWN_DATA,
+    NEW_DATA,
+    PROPOSAL,
     RUNS,
+    DatabaseVariable,
+    get_values,
 )
 from .utils import create_run_variables
 
-NEW_RUN = 100
+NEW_RUN = 400
 
 
 patched_sleep = patch.object(asyncio, "sleep", return_value=None)
@@ -35,12 +37,16 @@ def mocked_latest_rows(mocker, current_timestamp):
         "damnit_api.graphql.subscriptions.async_table",
         return_value=table_sentinel,
     )
+    mocker.patch(
+        "damnit_api.graphql.subscriptions.async_max",
+        return_value=0,
+    )
 
     def mocked_returns(*args, table, **kwargs):
         if table is table_sentinel:
             return create_run_variables(
-                NEW_VALUES,
-                proposal=KNOWN_VALUES["proposal"],
+                get_values(NEW_DATA),
+                proposal=PROPOSAL,
                 run=NEW_RUN,
                 timestamp=current_timestamp,
             )
@@ -53,18 +59,10 @@ def mocked_latest_rows(mocker, current_timestamp):
 
 
 @pytest.fixture
-def mocked_variables(mocker):
-    return mocker.patch(
-        "damnit_api.graphql.subscriptions.async_variables",
-        return_value=EXAMPLE_VARIABLES,
-    )
-
-
-@pytest.fixture
 def mocked_fetch_info(mocker):
     return mocker.patch(
         "damnit_api.graphql.subscriptions.fetch_info",
-        return_value=[{**KNOWN_VALUES, "run": NEW_RUN}],
+        return_value=[{**get_values(KNOWN_DATA), "run": NEW_RUN}],
     )
 
 
@@ -74,7 +72,6 @@ async def test_latest_data(
     current_timestamp,
     mocked_latest_rows,
     mocked_fetch_info,
-    mocked_variables,
 ):
     subscription = await graphql_schema.subscribe(
         """
@@ -85,29 +82,32 @@ async def test_latest_data(
         }
         """,
         variable_values={
-            "proposal": "1234",
+            "proposal": str(PROPOSAL),
             "timestamp": (current_timestamp - 1) * 1000,  # before the new row
-        },
-        context_value={
-            "schema": graphql_schema,
         },
     )
 
     result = await asyncio.wait_for(anext(subscription), timeout=2)
     assert not result.errors
 
-    data = {**KNOWN_VALUES, **NEW_VALUES, "run": NEW_RUN}
-    dtypes = {**KNOWN_DTYPES, **NEW_DTYPES}
+    data = {
+        **KNOWN_DATA,
+        **NEW_DATA,
+        "run": DatabaseVariable(
+            value=NEW_RUN,
+            damnit_dtype=DamnitType.NUMBER,
+        ),
+    }
 
     latest_data = result.data["latest_data"]
     assert set(latest_data.keys()) == {"runs", "metadata"}
     assert latest_data["runs"] == {
         NEW_RUN: {
             name: {
-                "value": serialize(value, dtype=dtypes[name])[0],
-                "dtype": dtypes[name].value,
+                "value": var.damnit_value,
+                "dtype": var.damnit_dtype.value,
             }
-            for name, value in data.items()
+            for name, var in data.items()
         }
     }
 
@@ -127,7 +127,6 @@ async def test_latest_data_with_concurrent_subscriptions(
     current_timestamp,
     mocked_latest_rows,
     mocked_fetch_info,
-    mocked_variables,
 ):
     query = """
         subscription LatestDataSubscription(
@@ -137,22 +136,17 @@ async def test_latest_data_with_concurrent_subscriptions(
         }
         """
     variables = {
-        "proposal": "1234",
+        "proposal": str(PROPOSAL),
         "timestamp": (current_timestamp - 1) * 1000,  # before the new row
-    }
-    context = {
-        "schema": graphql_schema,
     }
 
     first_sub = await graphql_schema.subscribe(
         query,
         variable_values=variables,
-        context_value=context,
     )
     second_sub = await graphql_schema.subscribe(
         query,
         variable_values=variables,
-        context_value=context,
     )
 
     with patched_sleep:
@@ -174,7 +168,6 @@ async def test_latest_data_with_nonconcurrent_subscriptions(
     current_timestamp,
     mocked_latest_rows,
     mocked_fetch_info,
-    mocked_variables,
 ):
     query = """
         subscription LatestDataSubscription(
@@ -184,22 +177,17 @@ async def test_latest_data_with_nonconcurrent_subscriptions(
         }
         """
     variables = {
-        "proposal": "1234",
+        "proposal": str(PROPOSAL),
         "timestamp": (current_timestamp - 1) * 1000,  # before the new row
-    }
-    context = {
-        "schema": graphql_schema,
     }
 
     first_sub = await graphql_schema.subscribe(
         query,
         variable_values=variables,
-        context_value=context,
     )
     second_sub = await graphql_schema.subscribe(
         query,
         variable_values=variables,
-        context_value=context,
     )
 
     with patched_sleep:
