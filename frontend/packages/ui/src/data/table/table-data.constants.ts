225ebf0bea2f7b7355bd78208ee29d3cddef06c1
Cammille Carinan <cammille.carinan@xfel.eu>
2026-07-16 14:33:38 +0200

    refactor(frontend/graphql): move table query names to a leaf module

--- frontend/packages/ui/src/data/table/table-data.constants.ts
+++ frontend/packages/ui/src/data/table/table-data.constants.ts
@@ -1,3 +1,6 @@
-export const TABLE_DATA_QUERY_NAME = 'TableDataQuery'
-export const DEFERRED_TABLE_DATA_QUERY_NAME = `Deferred${TABLE_DATA_QUERY_NAME}`
+export {
+  TABLE_DATA_QUERY_NAME,
+  DEFERRED_TABLE_DATA_QUERY_NAME,
+} from '../../graphql/operation-names'
+
 export const LATEST_DATA_FIELD_NAME = 'latest_data'
---
+++ frontend/packages/ui/src/graphql/operation-names.ts
@@ -0,0 +1,2 @@
+export const TABLE_DATA_QUERY_NAME = 'TableDataQuery'
+export const DEFERRED_TABLE_DATA_QUERY_NAME = `Deferred${TABLE_DATA_QUERY_NAME}`
